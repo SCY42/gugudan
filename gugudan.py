880e0771b40134ecd9c7f5d173ce2910a51ea338
42 <nooyeahc1125@gmail.com>
2023-05-29 17:31:38 +0900

    안내 문구 (제대로) 작성, 비정상적인 값 입력 시 예외 처리 등

--- gugudan.py
+++ gugudan.py
@@ -3,12 +3,19 @@ import time, random, csv, re
 # 메인
 
 def gugudan():
-    print("메인 메뉴 환영문구")
-    print("게임모드, 리더보드, 환경설정 메뉴")
-    player_input = input("선택문구: ")
+    print(""" ■■■■■   ■■■■■  ■■■■  ■
+     ■       ■  ■     ■■
+■■■■■■■ ■■■■■■■ ■■■■  ■
+   ■       ■     ■
+   ■       ■     ■■■■■■""")
+    print("")
+    print("타임어택 모드 - t\n스코어링 모드 - s\n리더보드 - l\n환경설정 - c\n게임 종료 - q")
+    print("")
+    player_input = input("입력하세요... ")
     while player_input not in ["t", "s", "c", "l", "q"]:
-        print("잘못된 입력")
-        player_input = input("선택문구: ")
+        print("잘못된 입력입니다!")
+        print("")
+        player_input = input("입력하세요... ")
     menu_redirection(player_input)
 
 
@@ -16,6 +23,7 @@ def gugudan():
 
 def menu_redirection(player_input):
     global timeattack_question, scoring_time, dan_number  # 함수 밖 변수 사용 선언
+    print("")
     if player_input == "t":
         timeattack(timeattack_question, dan_number)
     elif player_input == "s":
@@ -25,6 +33,8 @@ def menu_redirection(player_input):
     elif player_input == "l":
         leaderboard()
     elif player_input == "q":
+        print("안녕히 가세요!")
+        time.sleep(1)
         quit() # 프로그램 종료
     else:
         print("잘못된 입력")
@@ -38,31 +48,68 @@ dan_number = 9
 
 def configuration():
     global timeattack_question, scoring_time, dan_number  # 함수 밖 변수 사용 선언
-    print("환경설정 설명 (설정 바꾸면 랭킹등록 안됨)")
-    print("현재 설정", timeattack_question, scoring_time)
+    print("=====CONFIG=====")
+    print("")
+    print("타임어택 모드 문제 수 변경하기 - t")
+    print("스코어링 모드 제한 시간 변경하기 - s")
+    print("구구단 단수 변경하기 - n")
+    print("메인 메뉴로 돌아가기 - r")
+    print("")
+    print(f"현재 설정 - 타임어택 모드 문제 수: {timeattack_question}문제 (기본 20) / 스코어링 모드 제한 시간: {scoring_time}초 (기본 30) / 구구단 단수: {dan_number}단 (기본 9)")
+    print("")
 
-    option = input("설정할 항목 (r 입력해 메뉴로 돌아가기): ")
+    option = input("설정할 항목... ")
     while option not in ["t", "s", "n", "r"]:
-        print("잘못된 입력")
-        option = input("설정할 게임모드 선택: ")
+        print("잘못된 입력입니다!")
+        print("")
+        option = input("설정할 항목... ")
 
     if option == "t":
-        print("현재 설정 / 기본 설정")
-        timeattack_question = int(input("타임어택 모드 문제 수: "))
+        try:
+            timeattack_question = int(input("타임어택 모드 문제 수... "))
+            if timeattack_question < 0:
+                print("잘못된 입력입니다!")
+                timeattack_question = 20
+        except:
+            print("잘못된 입력입니다!")
+        print("")
         configuration()
     elif option == "s":
-        print("현재 설정 / 기본 설정")
-        scoring_time = int(input("스코어링 모드 제한 시간(초): "))
+        try:
+            scoring_time = int(input("스코어링 모드 제한 시간... "))
+            if scoring_time < 0:
+                print("잘못된 입력입니다!")
+                scoring_time = 30
+        except:
+            print("잘못된 입력입니다!")
+        print("")
         configuration()
     elif option == "n":
-        print("현재 설정 / 기본 설정")
-        dan_number = int(input("문제에 등장할 수 있는 최대 숫자: "))
+        try:
+            dan_number = int(input("구구단 단수... "))
+            if dan_number < 1:
+                print("잘못된 입력입니다!")
+                dan_number = 9
+
+        except:
+            print("잘못된 입력입니다!")
+        print("")
         configuration()
     elif option == "r":
+        print("")
         gugudan()
     else:
         print("잘못된 입력")
 
+
+# 기본값 체크
+
+def default():
+    if timeattack_question == 20 and scoring_time == 30 and dan_number == 9:
+        return True
+    else:
+        return False
+    
 
 # 3초 카운트다운
 
@@ -72,6 +119,7 @@ def countdown():
         print(f"{i}...")
         time.sleep(1)
     print("시작!")
+    print("")
 
 
 # 구구단 문제 생성
@@ -79,7 +127,10 @@ def countdown():
 def actual_gugudan(dan_number, q):
     a, b = random.randint(1, dan_number), random.randint(1, dan_number)
     answer = a * b
-    player_input = int(input(f"{q}. {a} x {b} = "))
+    try:  # 입력이 숫자가 아닐 경우 에러 방지
+        player_input = int(input(f"{q}. {a} x {b} = "))
+    except:
+        player_input = 0
     print("")
     if player_input == answer:
         return True
@@ -92,8 +143,10 @@ def actual_gugudan(dan_number, q):
 def timeattack(timeattack_question, dan_number):
     score = 0
     penalty = 0
-
-    print("타임어택 규칙 어쩌구저쩌구")
+    
+    print("=====TIMEATTACK=====")
+    print("")
+    print(f"최대한 빠르게 {timeattack_question}문제를 풀어내세요!")
     countdown()
     time_start = time.perf_counter()
     for q in range(1, timeattack_question+1):
@@ -117,6 +170,11 @@ def timeattack(timeattack_question, dan_number):
         print(f"소요 시간: {round(time_end - time_start, 3)}초 + 페널티 {penalty}초 = {round(time_end - time_start, 3) + penalty}초")
     
     time.sleep(3)
+    if default():
+        yn = input("y를 눌러 이 게임의 기록을 리더보드에 등록하기... ")
+        if yn == "y":
+            leaderboard_register("t", round(time_end - time_start, 3) + penalty)
+    print("")
     gugudan()
     
 
@@ -127,7 +185,9 @@ def scoring(scoring_time, dan_number):
     wrong = 0
     q = 1
 
-    print("스코어링 규칙 어쩌구저쩌구")
+    print("=====SCORING=====")
+    print("")
+    print(f"{scoring_time}초 안에 최대한 많은 문제를 풀어내세요!")
     countdown()
     time_limit = time.time() + scoring_time  # 시간 제한 = 현재 시간 + 설정된 제한시간
     while time.time() < time_limit:  # 마지막 문제의 답을 입력하기 전까지 종료되지 않으나, 달리 해결할 방도가 없음
@@ -145,6 +205,14 @@ def scoring(scoring_time, dan_number):
         print("전부 맞혔습니다! 훌륭해요!")
     elif correct == 0:
         print("전부 틀렸습니다! (더 빠른 연타로 최저 점수에 도전해보세요)")
+
+    time.sleep(3)
+    if default() and correct - wrong >= 0:
+        yn = input("y를 눌러 이 게임의 기록을 리더보드에 등록하기... ")
+        if yn == "y":
+            leaderboard_register("s", correct - wrong)
+    print("")
+    gugudan()
 
 
 # 리더보드
@@ -181,19 +249,26 @@ def leaderboard():
     for i in range(5):
         ranking.append((timeattack_result_list[i], scoring_result_list[i]))
 
-    print("   TIMEATTACK    |   SCORING ")
-    print("===============================")
+    print("       TIMEATTACK    |   SCORING ")
+    print("===================================")
+    rank = ["1st", "2nd", "3rd", "4th", "5th"]
+    rank_n = 0
     for i in ranking:
-        print(i[0][0], i[0][1], "|", i[1][0], i[1][1])
+        print(rank[rank_n], i[0][0], i[0][1], "|", i[1][0], i[1][1])
+        rank_n += 1
+    print("")
+    back = input("Enter를 눌러 메인 메뉴로 돌아가기... ")
+    print("")
+    gugudan()
 
 
 # 스코어 집계
 
 def leaderboard_register(gamemode, result):
     username_format = re.compile("[a-zA-Z0-9]+")
-    username = input("유저네임을 입력하세요(영문, 숫자): ")
+    username = input("유저네임을 입력하세요(영문, 숫자)... ")
     while not username_format.match(username):
-        username = input("유저네임을 입력하세요(영문, 숫자): ")
+        username = input("유저네임을 입력하세요(영문, 숫자)... ")
     username = f"{username:<8}"
 
     if gamemode == "t":
@@ -212,4 +287,4 @@ def leaderboard_register(gamemode, result):
 
 # 구구단 실행
 
-leaderboard()
+gugudan()
